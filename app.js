10b6dcec971bc5e21916cbb5dbc02e5485775fdd
Maji <start99start@gmail.com>
2023-02-27 13:45:02 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
-
+const bodyParser = require('body-parser')// 引用 body-parser
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV != 'production') {
   require('dotenv').config()
@@ -28,12 +28,28 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 // extname:'.hbs'，是指定副檔名為.hbs，有了這行才能把預設長檔名改寫成短檔名
 app.set('view engine', 'hbs')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error))// 錯誤處理
+    .catch(error => console.log(error))// 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name: name }) // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+
+
 })
 
 // 設定 port 3000
