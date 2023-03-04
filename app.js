1c5e81ab2bce99990c551ca96729a59988935e11
Maji <start99start@gmail.com>
2023-03-04 17:35:07 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -35,9 +35,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index 樣板
     .catch(error => console.log(error))// 錯誤處理
 })
+
+// 設定new頁面路由
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
 // 設定detail頁面路由
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
@@ -53,11 +60,6 @@ app.get('/todos/:id/edit', (req, res) => {
     .lean()
     .then((todo) => res.render('edit', { todo: todo }))
     .catch(error => console.log(error))
-})
-
-// 設定new頁面路由
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
 })
 
 //設定路由來接住表單新增資料，並且把資料送往資料庫
